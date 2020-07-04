0a9e0d2dff5455c11e6d3d3b94eb498a8351e115
dogyeong <shroad1802@naver.com>
2020-07-05 00:54:00 +0900

    Follow / Unfollow Users from Profile Page

--- components/profile/FollowUser.js
+++ components/profile/FollowUser.js
@@ -1,7 +1,8 @@
-import {Button} from 'antd'
+import { Button } from 'antd'
+import { followUser, unfollowUser } from '../../lib/api'
 
-export default const FollowUser = () => {
-  return (
-    <Button></Button>
-  )
+export default function FollowUser({ isFollowing, toggleFollow }) {
+  const request = isFollowing ? unfollowUser : followUser
+
+  return <Button onClick={() => toggleFollow(request)}>{isFollowing ? '언팔로우' : '팔로우'}</Button>
 }
--- pages/profile.js
+++ pages/profile.js
@@ -3,23 +3,36 @@ import { getUser } from '../lib/api'
 import { useEffect, useState } from 'react'
 import Link from 'next/link'
 import { Skeleton, Card, Avatar, Layout, Button, Divider } from 'antd'
+import FollowUser from '../components/profile/FollowUser'
 const { Meta } = Card
 
 export default function Profile({ auth, userId }) {
   const [user, setUser] = useState({})
   const [isAuth, setIsAuth] = useState(false)
   const [isLoading, setIsLoading] = useState(true)
+  const [isFollowing, setIsFollowing] = useState(false)
 
   useEffect(() => {
     const isAuth = auth.user._id === userId
     setIsAuth(isAuth)
     ;(async function () {
       getUser(userId).then((user) => {
+        setIsFollowing(checkFollow(auth, user))
         setUser(user)
         setIsLoading(false)
       })
     })()
   }, [])
+
+  const checkFollow = (auth, user) => {
+    return user.followers.findIndex((follower) => follower._id === auth.user._id) > -1
+  }
+
+  const toggleFollow = (sendRequest) => {
+    sendRequest(userId).then(() => {
+      setIsFollowing(!isFollowing)
+    })
+  }
 
   return (
     <Layout style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
@@ -35,7 +48,7 @@ export default function Profile({ auth, userId }) {
               </Link>
             </Button>
           ) : (
-            <Button>팔로우</Button>
+            <FollowUser isFollowing={isFollowing} toggleFollow={toggleFollow} />
           )
         }
       >
