78af6c6140b2acde7b411386a03af28b591fc7bd
dogyeong <shroad1802@naver.com>
2020-07-07 02:30:35 +0900

    Deploy to Heroku

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,27 @@
 import { authInitialProps } from '../lib/auth'
 import Link from 'next/link'
-import { Button } from 'antd'
+import { Button, Layout } from 'antd'
 import { ArrowRightOutlined } from '@ant-design/icons'
+const { Content, Sider } = Layout
 
 export default function Index({ auth }) {
   return (
     <React.Fragment>
       {auth.user && auth.user._id ? (
-        <div>auth user page</div>
+        <Layout>
+          <Content>POST FEED</Content>
+          <Sider theme="light">asd</Sider>
+        </Layout>
       ) : (
         <div className="landing-container">
           <div className="landing-left">
             <h1 className="landing-title">오늘, 모목지?</h1>
             <p className="landing-text">뭘 먹을지 고민일 때, 한번 둘러보세요!</p>
-            <Button type="primary" shape="round" icon={<ArrowRightOutlined />}>
-              <Link href="/signin">
+            <Link href="/signin">
+              <Button type="primary" shape="round" icon={<ArrowRightOutlined />}>
                 <a style={{ color: 'white', marginLeft: 8 }}>시작하기</a>
-              </Link>
-            </Button>
+              </Button>
+            </Link>
           </div>
           <div className="landing-right">sdaasd</div>
         </div>
@@ -28,6 +32,7 @@ export default function Index({ auth }) {
           flex: 1;
           align-self: center;
           display: flex;
+          flex-direction: row;
           justify-content: space-between;
           padding-bottom: 15vh;
         }
@@ -42,6 +47,19 @@ export default function Index({ auth }) {
         }
         .landing-right {
           flex: 1;
+        }
+        @media screen and (max-width: 700px) {
+          .landing-container {
+            flex-direction: column-reverse;
+            padding-bottom: 10vh;
+          }
+          .landing-left {
+            text-align: center;
+          }
+          .landing-right {
+            text-align: center;
+            margin-bottom: 40px;
+          }
         }
       `}</style>
     </React.Fragment>
