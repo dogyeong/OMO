56bdb46d5549b4de0d2139dbe0be02adb0127367
dogyeong <shroad1802@naver.com>
2020-08-25 22:21:40 +0900

    update: use s3 when adding post

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,7 +1,10 @@
-const multer = require('multer')
-const jimp = require('jimp')
-const mongoose = require('mongoose')
-const Post = mongoose.model('Post')
+const multer = require('multer');
+const jimp = require('jimp');
+const mongoose = require('mongoose');
+const Post = mongoose.model('Post');
+const AWS = require('aws-sdk');
+
+AWS.config.update({ region: 'us-east-2' });
 
 const imageUploadOptions = {
   storage: multer.memoryStorage(),
@@ -11,98 +14,137 @@ const imageUploadOptions = {
   },
   fileFilter: (req, file, next) => {
     if (file.mimetype.startsWith('image/')) {
-      next(null, true)
+      next(null, true);
     } else {
-      next(null, false)
+      next(null, false);
     }
   },
-}
+};
 
-exports.uploadImage = multer(imageUploadOptions).single('image')
+exports.uploadImage = multer(imageUploadOptions).single('image');
 
 exports.resizeImage = async (req, res, next) => {
   if (!req.file) {
-    return next()
+    return next();
   }
-  const extension = req.file.mimetype.split('/')[1]
-  req.body.image = `/uploads/${req.user.name}-${Date.now()}.${extension}`
-  const image = await jimp.read(req.file.buffer)
-  await image.resize(750, jimp.AUTO)
-  await image.write(`./public${req.body.image}`)
-  next()
-}
+
+  const image = await jimp.read(Buffer.from(req.file.buffer, 'base64'));
+  await image.resize(750, jimp.AUTO);
+  const file = await image.getBufferAsync(jimp.AUTO);
+
+  const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: 'us-east-2',
+  });
+
+  const extension = req.file.mimetype.split('/')[1];
+
+  const params = {
+    Bucket: process.env.S3_BUCKET_NAME,
+    Key: `uploads/${req.user.name}-${Date.now()}.${extension}`,
+    Body: file,
+    ContentType: req.file.mimetype,
+    ACL: 'public-read',
+  };
+
+  try {
+    await new Promise((resolve, reject) => {
+      return s3.upload(params, (err, data) => {
+        if (err) {
+          console.log(err);
+          return reject(err);
+        }
+        console.log(data);
+        req.body.image = data.Location;
+        resolve(next());
+      });
+    });
+  } catch (err) {
+    throw new Error(err);
+  }
+
+  // 기존 express에 static file upload
+  //
+  // const extension = req.file.mimetype.split('/')[1];
+  // req.body.image = `/uploads/${req.user.name}-${Date.now()}.${extension}`;
+  // const image = await jimp.read(req.file.buffer);
+  // await image.resize(750, jimp.AUTO);
+  // await image.write(`./public${req.body.image}`);
+  // next();
+};
 
 exports.addPost = async (req, res) => {
-  req.body.postedBy = req.user._id
-  const post = await new Post(req.body).save()
+  req.body.postedBy = req.user._id;
+  const post = await new Post(req.body).save();
   await Post.populate(post, {
     path: 'postedBy',
     select: '_id name avatar',
-  })
-  res.json(post)
-}
+  });
+  res.json(post);
+};
 
 exports.getPostById = async (req, res, next, id) => {
-  const post = await Post.findOne({ _id: id })
-  req.post = post
+  const post = await Post.findOne({ _id: id });
+  req.post = post;
 
-  const posterId = mongoose.Types.ObjectId(req.post.postedBy._id)
+  const posterId = mongoose.Types.ObjectId(req.post.postedBy._id);
   if (req.user && posterId.equals(req.user._id)) {
-    req.isPoster = true
-    return next()
+    req.isPoster = true;
+    return next();
   }
-  next()
-}
+  next();
+};
 
 exports.deletePost = async (req, res) => {
-  const { _id } = req.post
+  const { _id } = req.post;
 
   if (!req.isPoster) {
     return res.status(400).json({
       message: 'You are not authorized to perform this action',
-    })
+    });
   }
-  const deletedPost = await Post.findOneAndDelete({ _id })
-  res.json(deletedPost)
-}
+  const deletedPost = await Post.findOneAndDelete({ _id });
+  res.json(deletedPost);
+};
 
 exports.getPostsByUser = async (req, res) => {
-  const posts = await Post.find({ postedBy: req.profile._id }).sort({ createdAt: 'desc' })
-  res.json(posts)
-}
+  const posts = await Post.find({ postedBy: req.profile._id }).sort({ createdAt: 'desc' });
+  res.json(posts);
+};
 
 exports.getPostFeed = async (req, res) => {
-  const { following, _id } = req.profile
-  following.push(_id)
-  const posts = await Post.find({ postedBy: { $in: following } }).sort({ createdAt: 'desc' })
-  res.json(posts)
-}
+  const { following, _id } = req.profile;
+  following.push(_id);
+  const posts = await Post.find({ postedBy: { $in: following } }).sort({ createdAt: 'desc' });
+  res.json(posts);
+};
 
 exports.toggleLike = async (req, res) => {
-  const { postId } = req.body
-  const post = await Post.findOne({ _id: postId })
-  const likeIds = post.likes.map((id) => id.toString())
-  const authUserId = req.user._id.toString()
+  const { postId } = req.body;
+  const post = await Post.findOne({ _id: postId });
+  const likeIds = post.likes.map((id) => id.toString());
+  const authUserId = req.user._id.toString();
   if (likeIds.includes(authUserId)) {
-    await post.likes.pull(authUserId)
+    await post.likes.pull(authUserId);
   } else {
-    await post.likes.push(authUserId)
+    await post.likes.push(authUserId);
   }
-  await post.save()
-  res.json(post)
-}
+  await post.save();
+  res.json(post);
+};
 
 exports.toggleComment = async (req, res) => {
-  const { comment, postId } = req.body
-  let operator
-  let data
+  const { comment, postId } = req.body;
+  let operator;
+  let data;
 
   if (req.url.includes('uncomment')) {
-    operator = '$pull'
-    data = { _id: comment._id }
+    operator = '$pull';
+    data = { _id: comment._id };
   } else {
-    operator = '$push'
-    data = { text: comment.text, postedBy: req.user._id }
+    operator = '$push';
+    data = { text: comment.text, postedBy: req.user._id };
   }
 
   const updatedPost = await Post.findOneAndUpdate(
@@ -111,7 +153,7 @@ exports.toggleComment = async (req, res) => {
     { new: true }
   )
     .populate('postedBy', '_id name avatar')
-    .populate('comments.postedBy', '_id name avatar')
+    .populate('comments.postedBy', '_id name avatar');
 
-  res.json(updatedPost)
-}
+  res.json(updatedPost);
+};
